ff3cdc798c3642a85272633e466c4d9b49acd2cb
shashankvidiyala <s19v1997@gmail.com>
2022-11-12 14:28:39 -0500

    ui sos button and maps

--- client/src/Landing.jsx
+++ client/src/Landing.jsx
@@ -6,16 +6,27 @@ import IconButton from '@mui/material/IconButton';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 import InsightsIcon from '@mui/icons-material/Insights';
 import './Landing.css';
+import { sendSOS } from './Services/SendSOS';
+import { useState } from 'react';
+import {
+    Link
+  } from "react-router-dom";
 
-export const LandingPage = () => <Container>
-    <div>
-        <section className='lp-header'>    <IconButton size="lg"><ManageAccountsIcon></ManageAccountsIcon></IconButton>
-        <IconButton size="lg"><InsightsIcon></InsightsIcon></IconButton>
-</section>
-        <section className='lp-sos'><Button variant="danger" size="xlg">
-SOS        </Button></section>
-<section className='lp-sec'><Button variant="success"  disabled="true" size="xlg">
-Mark Safe        </Button></section>
-    </div>
-    
-</Container>;
+export const LandingPage = () => {
+    const [sos, setSos] = useState(false);
+    return <Container>
+        <div>
+            <section className='lp-header'>   <Link to="/account/"> <IconButton size="lg"><ManageAccountsIcon></ManageAccountsIcon></IconButton></Link>
+                <Link to="/map/"><IconButton size="lg"><InsightsIcon></InsightsIcon></IconButton></Link>
+            </section>
+            <section className='lp-sos'><Button variant="danger" onClick={async (e) => {
+                const resp = await sendSOS('john', navigator.geolocation, Date.now());
+                setSos(resp)
+            }} size="xlg">
+                SOS        </Button></section>
+            <section className='lp-sec'><Button variant="success" disabled={true} size="xlg">
+                Mark Safe        </Button></section>
+        </div>
+
+    </Container>;
+}
